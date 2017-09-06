f2d160e0e6ec0437cdd77be419997a7b3c7ff5c1
RoboChargers <cchsrobochargers@gmail.com>
2017-09-05 21:08:37 -0400

    Added new files

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/SwitchActivity.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.qualcomm.ftcrobotcontroller.R;
+
+public class SwitchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_switch);
+    }
+}
