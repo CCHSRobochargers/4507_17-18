2b4fdee92818f243e1f5d6f11a5084bd07bc8c48
RoboChargers <cchsrobochargers@gmail.com>
2017-09-07 21:36:19 -0400

    computer is absolutely screwed up, switching computers again

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/CreateAuto.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/CreateAuto.java
@@ -6,10 +6,18 @@ import android.os.Bundle;
 import com.qualcomm.ftcrobotcontroller.R;
 
 public class CreateAuto extends Activity {
+    METHODS methods;
+    METHODS.MoveStraightParams moveStraightParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_auto);
+
+        methods = new METHODS();
+        moveStraightParams = methods.new MoveStraightParams();
+
+        moveStraightParams.promptParams(this, -1.0, 1.0);
+
     }
 }
